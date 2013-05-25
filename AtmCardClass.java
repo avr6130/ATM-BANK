281bda2e8e925d983ac8e8f0985c48e519fab02e
Anthony Ricco <avr6130@comcast.net>
2013-05-25 15:00:45 -0400

    Initial creation

---
+++ AtmCardClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: aricco
+ * Date: 5/25/13
+ * Time: 1:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AtmCardClass {
+}
