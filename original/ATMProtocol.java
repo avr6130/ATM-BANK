c343cda79d6a6706a6d0d6a563b83cbf1c96b966
mnorr9 <michaelv.norris@gmail.com>
2013-05-31 13:34:45 -0400

    cleaned up atm request messages

--- original/ATMProtocol.java
+++ original/ATMProtocol.java
@@ -1,9 +1,21 @@
 package original;
 
-import messaging.*;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+
+import messaging.AuthenticationRequest;
+import messaging.AuthenticationResponse;
+import messaging.BalanceRequest;
+import messaging.BalanceResponse;
+import messaging.Message;
+import messaging.MessageHandler;
+import messaging.Payload;
+import messaging.WithdrawRequest;
+import messaging.WithdrawResponse;
 
 /**
  * An ATMProtocol processes local splitStr[0]s sent to the ATM and writes to or reads
@@ -36,80 +48,22 @@ public class ATMProtocol implements Protocol {
         stdIn.close();
     }
 
-    private void processBeginSession(String[] splitCmdString) throws IOException {
-        Message msg = new Message();
-
-        try {
-            authenticationRequest = atmTransactionManager.authenticateSession(splitCmdString);
-
-            if (authenticationRequest == null)
-                System.out.println("Unauthorized");
-            else {
-
-                msg.setPayload(authenticationRequest);
-                writer.writeObject(msg);
-
-                // After the message is set to bank, prepare to process the response and block
-                processRemoteCommands();
-            } // end else
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            e.getMessage();   //To change body of catch statement use File | Settings | File Templates.
-        } // end catch
-    } // end processBeginSession()
-
-    private void processRequestBalance() throws IOException {
-    	
-    	if (!atmTransactionManager.transactionActive()) {
-            System.out.println("No user logged in");
-            return;
-        }
-    	
-        Message msg = new Message();
-        BalanceRequest balanceRequest = new BalanceRequest(atmTransactionManager.getActiveAccountNum());
-
-        try {
-
-            msg.setPayload(balanceRequest);
-            writer.writeObject(msg);
-
-            // After the message is set to bank, prepare to process the response and block
-            processRemoteCommands();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            e.getMessage();   //To change body of catch statement use File | Settings | File Templates.
-        } // end catch
-
-        System.out.println("balance successfully requested");
-
-    } // end processRequestBalance()
-
     private void processCommand(String command) throws IOException {
 
         // Split the input command on whitespace
         String[] splitCmdString = command.split("\\s+");
+        
+        Message requestMessage = null;
 
         if (splitCmdString[0].toLowerCase().matches("begin-session")) {
-
-            if (atmTransactionManager.transactionActive()) {
-                System.out.println("Transaction currently in progress.  Please end-session before beginning a new session.");
-            } // end if transactionActive()
-            else {
-                processBeginSession(splitCmdString);
-            } // end else
+        	requestMessage = this.generateAuthenticationRequest(splitCmdString);
 
         } // end if begin-session request
-
         else if (splitCmdString[0].toLowerCase().matches("balance")) {
-        	processRequestBalance();
-
+        	requestMessage = this.generateBalanceRequest();
         } // end else if balance request
-
         else if (splitCmdString[0].toLowerCase().matches("withdraw")) {
-            processRequestWithdraw();
-
+        	requestMessage = this.generateWithdrawRequest();  	
         } else if (splitCmdString[0].toLowerCase().matches("end-session")) {
 
             atmTransactionManager.endCurrentTransaction();
@@ -119,43 +73,81 @@ public class ATMProtocol implements Protocol {
         else {
             System.out.println("Illegal input entered");
         } // end else
+        
+        try {
+        	if (requestMessage != null) {
+	            writer.writeObject(requestMessage);
+	
+	            // After the message is set to bank, prepare to process the response and block
+	            processRemoteCommands();
+        	}
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            e.getMessage();   //To change body of catch statement use File | Settings | File Templates.
+        } // end catch
 
     } // end processCommand
 
-    private void processRequestWithdraw() {
+    private Message generateAuthenticationRequest(String[] splitCmdString) throws IOException {
     	
+    	Message msg = null;
+    	if (atmTransactionManager.transactionActive()) {
+            System.out.println("Transaction currently in progress.  Please end-session before beginning a new session.");
+            return msg;
+        } // end if transactionActive()
+        
+    	authenticationRequest = atmTransactionManager.authenticateSession(splitCmdString);
+
+    	if (authenticationRequest == null) {
+    		System.out.println("Unauthorized");
+    		return msg;
+    	} else {
+    		msg = new Message();
+    		msg.setPayload(authenticationRequest);
+    		return msg;
+    	}
+ 
+	}
+
+	private Message generateWithdrawRequest() {
+		Message msg = null;
+		
     	if (!atmTransactionManager.transactionActive()) {
             System.out.println("No user logged in");
-            return;
+            return msg;
         }
     	
 		double amt = promptForWithdraw();
 		if (amt > 0) {
-			Message msg = new Message();
+			msg = new Message();
 	        WithdrawRequest withdrawRequest = new WithdrawRequest(atmTransactionManager.getActiveAccountNum(), amt);
 
-	        try {
-
-	            msg.setPayload(withdrawRequest);
-	            writer.writeObject(msg);
-
-	            // After the message is set to bank, prepare to process the response and block
-	            processRemoteCommands();
-
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	            e.getMessage();   //To change body of catch statement use File | Settings | File Templates.
-	        } // end catch
+	         msg.setPayload(withdrawRequest);
+	         return msg;
 		}
+		return msg;
+	}
+
+	private Message generateBalanceRequest() {
+		Message msg = null;
+		if (!atmTransactionManager.transactionActive()) {
+			System.out.println("No user logged in");
+			return msg;
+		}
+
+		BalanceRequest balanceRequest = new BalanceRequest(atmTransactionManager.getActiveAccountNum());
+		
+		msg = new Message();
+		msg.setPayload(balanceRequest);
+		return msg;
 	}
 
 	public void processRemoteCommands() throws IOException {
         Message msgObject;
 
         try {
-            //while ((msgObject = (Message)reader.readObject()) != null) {
             msgObject = (Message) reader.readObject();
-//            messageHandler.processMessage(msgObject);
 
             // Pull the payload out of the generic message.  The payload is the
             // specific message type.
